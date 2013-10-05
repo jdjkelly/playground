d718168c8387d708d968add0fd94ea03ee63002b
Joshua Kelly <jdjkelly@gmail.com>
2013-10-05 13:20:04 -0400

    Added saveHandler

--- wiki.go
+++ wiki.go
@@ -35,7 +35,11 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
   title := r.URL.Path[lenPath:]
-  p, _ := loadPage(title)
+  p, err := loadPage(title)
+  if err != nil {
+    http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+    return
+  }
   renderTemplate(w, "view", p)
 }
  
@@ -46,6 +50,14 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
     p = &Page{Title: title}
   }
   renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+  title := r.URL.Path[lenPath:]
+  body := r.FormValue("body")
+  p := &Page{Title: title, Body: []byte(body)}
+  p.save()
+  http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
 func main() {
